21a04e9fa2eb03b52b2780dd125249ce72d448e6
slumsd01 <90315740+slumsd01@users.noreply.github.com>
2022-02-20 14:41:57 -0500

    added offline functionality - information is saved & uploaded when app comes back online

--- public/js/idb.js
+++ public/js/idb.js
@@ -1,5 +1,4 @@
 let db;
-
 const request = indexedDB.open('budget_tracker', 1)
 
 // after version changes
@@ -15,7 +14,7 @@ request.onsuccess = function(event) {
     db = event.target.result
 
     if (navigator.onLine) {
-        // uploadTransaction()
+        uploadTransaction()
     }
 }
 
@@ -27,8 +26,47 @@ request.onerror = function(event) {
 // to submit a new transaction without internet connection
 function saveRecord(record) {
     const transaction = db.transaction(['new_transaction'], 'readwrite')
-
     const transactionObjectStore = transaction.objectStore('new_transaction')
-
     transactionObjectStore.add(record)
 }
+
+// upload transaction data
+function uploadTransaction() {
+    const transaction = db.transaction(['new_transaction'], 'readwrite')
+    const transactionObjectStore = transaction.objectStore('new_transaction')
+
+    // get records from store & set to variable
+    const getAll = transactionObjectStore.getAll()
+
+    // after succesfull getAll
+    getAll.onsuccess = function() {
+        // send data from store to api server
+        if (getAll.result.length > 0) {
+            fetch('/api/transaction', {
+                method: 'POST',
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: 'application/json, text/plain, */*',
+                    'Content-Type': 'application/json'
+                }
+            })
+            .then(response => response.json())
+            .then(serverResponse => {
+                if (serverResponse.message) {
+                    throw new Error(serverResponse)
+                }
+                // open db transaction again to clear store
+                const transaction = db.transaction(['new_transaction'], 'readwrite')
+                const transactionObjectStore = transaction.objectStore('new_transaction')
+                transactionObjectStore.clear()
+                alert('All saved transactions have been submitted.')
+            })
+            .catch(err => {
+                console.log(err)
+            })
+        }
+    }
+}
+
+// listen for app coming back online
+window.addEventListener('online', uploadTransaction)
